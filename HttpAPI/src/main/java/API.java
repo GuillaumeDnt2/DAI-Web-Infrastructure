ddb09d0db22fd0f6710233efb48d99d07324edec
vbonzon <113915093+vbonzon@users.noreply.github.com>
2023-12-14 11:30:53 +0100

    quel enfer

--- HttpAPI/src/main/java/API.java
+++ HttpAPI/src/main/java/API.java
@@ -1,5 +1,8 @@
-public class API{
-    public static void main (String[] args){
+import io.javalin.*;
 
+public class API{
+   
+    public static void main (String[] args){
+        System.out.println("TAMERE");
     }
 }
