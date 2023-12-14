d3df199c519c603067fccc3ef5498371a5ae4d9d
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2023-12-14 11:15:33 +0100

    ajout contry

---
+++ HttpAPI/src/main/java/Country.java
@@ -0,0 +1,12 @@
+public class Country {
+    public String name;
+    public String capital;
+    public int    population;
+    public String flagPath; //Si on a du temps
+
+    public Country(String name, String capital, int population){
+        this.name = name;
+        this.capital = capital;
+        this.population = population;
+    }
+}
