4afe489c92517cd055af9da8503f6e10e27a0cab
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2023-12-21 11:23:00 +0100

    Implémentation api

--- HttpAPI/src/main/java/CountryController.java
+++ HttpAPI/src/main/java/CountryController.java
@@ -5,16 +5,35 @@ public class CountryController {
     private ConcurrentHashMap<String, Country> countries = new ConcurrentHashMap<>();
 
     public CountryController(){
-        countries.put("Switzerland", new Country("Switzerland", "Bern", 9000000));
+        countries.put("Switzerland", new Country("Switzerland", "Bern", 8796669));
+        countries.put("France", new Country("France", "Paris", 64756584));
+        countries.put("India", new Country("India", "New Delhi", 1428627663));
     }
 
     public void get(Context ctx){
-        String countryName = ctx.pathParam("country");
+        String countryName = ctx.pathParam("name");
         ctx.json(countries.get(countryName));
     }
 
     public void getAll(Context ctx){
         ctx.json(countries);
+    }
+
+    public void create(Context ctx){
+        Country country = ctx.bodyAsClass(Country.class);
+        countries.put(country.name, country);
+        ctx.status(201);
+    }
+
+    public void delete(Context ctx){
+        String cntryName = ctx.pathParam("name");
+        countries.remove(cntryName);
+    }
+
+    public void update(Context ctx){
+        String cntryName = ctx.pathParam("name");
+        Country country = ctx.bodyAsClass(Country.class);
+        countries.put(cntryName, country);
     }
 
 }
